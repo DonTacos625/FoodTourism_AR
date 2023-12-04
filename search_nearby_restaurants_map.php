d2380a34b2046e9b9a2a640acc83c0dd0f308dd3
DonTacos625 <mhirano_0730@yahoo.co.jp>
2023-12-04 14:16:30 +0900

    18 commit

--- search_nearby_restaurants_map.php
+++ search_nearby_restaurants_map.php
@@ -391,26 +391,7 @@ if (
         };
         */
         function test() {
-            navigator.geolocation.getCurrentPosition(
-                test2,
-                // 取得失敗した場合
-                function(error) {
-                    switch (error.code) {
-                        case 1: //PERMISSION_DENIED
-                            alert("位置情報の利用が許可されていません");
-                            break;
-                        case 2: //POSITION_UNAVAILABLE
-                            alert("現在位置が取得できませんでした");
-                            break;
-                        case 3: //TIMEOUT
-                            alert("タイムアウトになりました");
-                            break;
-                        default:
-                            alert("その他のエラー(エラーコード:" + error.code + ")");
-                            break;
-                    }
-                }
-            );
+            navigator.geolocation.getCurrentPosition(test2);
         }
 
         function test2(position) {
